2e2632427bae34452b20cceea7c1aa7b21686f7a
Moshiul Arefin <marefin@globalintertech.com>
2013-02-03 18:10:18 -0600

    added test cases
    more test cases for the client input validation

--- src/ee382n/assignments/ticket_res/TestTicketReservationClient.java
+++ src/ee382n/assignments/ticket_res/TestTicketReservationClient.java
@@ -100,4 +100,62 @@ public class TestTicketReservationClient {
 		assertTrue(ticket3.seatNum == 5);		
 		
 	}
+
+	@Test public void userInputCheckTest0(){
+		TicketReservationClient ticket0 = new TicketReservationClient("reserve name");		
+		assertTrue(ticket0.checkUserInput() == true);
+		
+		TicketReservationClient ticket1 = new TicketReservationClient("bookseat name 5");
+		assertTrue(ticket1.checkUserInput() == true);
+		
+		TicketReservationClient ticket2 = new TicketReservationClient("search name");
+		assertTrue(ticket2.checkUserInput() == true);
+		
+		TicketReservationClient ticket3 = new TicketReservationClient("delete name");
+		assertTrue(ticket3.checkUserInput() == true);
+		
+	}
+	
+	@Test public void userInputCheckTest1(){
+		TicketReservationClient ticket0 = new TicketReservationClient("reserve <name>");
+		assertTrue(ticket0.checkUserInput() == true);
+		
+		TicketReservationClient ticket1 = new TicketReservationClient("bookseat <name> <5>");
+		assertTrue(ticket1.checkUserInput() == true);
+		
+		TicketReservationClient ticket2 = new TicketReservationClient("search <name>");
+		assertTrue(ticket2.checkUserInput() == true);
+		
+		TicketReservationClient ticket3 = new TicketReservationClient("delete <name>");
+		assertTrue(ticket3.checkUserInput() == true);
+	}
+	
+	@Test public void userInputCheckTest3(){
+		TicketReservationClient ticket0 = new TicketReservationClient("reserve");
+		assertTrue(ticket0.checkUserInput() == false);
+		
+		TicketReservationClient ticket1 = new TicketReservationClient("bookseat");
+		assertTrue(ticket1.checkUserInput() == false);
+		
+		TicketReservationClient ticket2 = new TicketReservationClient("search");
+		assertTrue(ticket2.checkUserInput() == false);
+		
+		TicketReservationClient ticket3 = new TicketReservationClient("delete");
+		assertTrue(ticket3.checkUserInput() == false);
+	}
+	
+	@Test public void userInputCheckTest4(){
+		TicketReservationClient ticket0 = new TicketReservationClient("reserve name 5");
+		assertTrue(ticket0.checkUserInput() == false);
+		
+		TicketReservationClient ticket1 = new TicketReservationClient("bookseat name 5");
+		assertTrue(ticket1.checkUserInput() == true);
+		
+		TicketReservationClient ticket2 = new TicketReservationClient("search name 5");
+		assertTrue(ticket2.checkUserInput() == false);
+		
+		TicketReservationClient ticket3 = new TicketReservationClient("delete name 5");
+		assertTrue(ticket3.checkUserInput() == false);
+	}
+
 }
