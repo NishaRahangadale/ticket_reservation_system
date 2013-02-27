a52f49fd128acbc6bcd2e69481212493d86dc84e
Moshiul Arefin <marefin@globalintertech.com>
2013-02-27 15:48:19 -0600

    Saved as UTF-8

--- src/ee382n/assignments/ticket_res/TicketReservationClient.java
+++ src/ee382n/assignments/ticket_res/TicketReservationClient.java
@@ -1,4 +1,4 @@
-/**
+ï»¿/**
  * @file	TicketReservationClient.java
  * 
  * @author 	Aaron Alaniz (aaron.a.alaniz@gmail.com)
@@ -51,20 +51,20 @@ public class TicketReservationClient {
 	
 	/*
 	 * 
-	 * 1. reserve <name> – inputs the name of a person and reserves a seat against this name. If the theater does not
-have enough seats(completely booked), no seat is assigned and the command responds with message: ‘Sold out -
-No seat available’. If a reservation has already been made under that name, then the command responds with
-message: ‘Seat already booked against the name provided’.
-Otherwise, a seat is reserved against the name provided and the client is relayed a message: ‘Seat assigned to you
-is <seat-number>’.
-2. bookSeat <name> <seatNum> – behaves similar to reserve command, but imposes an additional constraint that
+	 * 1. reserve <name> â€“ inputs the name of a person and reserves a seat against this name. If the theater does not
+have enough seats(completely booked), no seat is assigned and the command responds with message: â€˜Sold out -
+No seat availableâ€™. If a reservation has already been made under that name, then the command responds with
+message: â€˜Seat already booked against the name providedâ€™.
+Otherwise, a seat is reserved against the name provided and the client is relayed a message: â€˜Seat assigned to you
+is <seat-number>â€™.
+2. bookSeat <name> <seatNum> â€“ behaves similar to reserve command, but imposes an additional constraint that
 a seat is reserved if and only if there is no existing reservation against name and the seat having the number seatNum
-is available. If there is no existing reservation but seatNum is not available, the response is: ‘<seatNum> is not
-available’.
-3. search <name> – returns the seat number reserved for name. If no reservation is found for name the system responds
-with a message: ‘No reservation found for <name>’.
-4. delete <name> – frees up the seat allocated to that person. The command returns the seat number that was
-released. If no existing reservation was found, responds with: ‘No reservation found for <name>’.
+is available. If there is no existing reservation but seatNum is not available, the response is: â€˜<seatNum> is not
+availableâ€™.
+3. search <name> â€“ returns the seat number reserved for name. If no reservation is found for name the system responds
+with a message: â€˜No reservation found for <name>â€™.
+4. delete <name> â€“ frees up the seat allocated to that person. The command returns the seat number that was
+released. If no existing reservation was found, responds with: â€˜No reservation found for <name>â€™.
 Here <text> is used to indicate the value of the parameter denoted by text in the given context. Note that you
 also have to write the client program that takes input from the user and then communicates with a server using sockets.
 Your program should behave correctly in presence of multiple concurrent clients.
